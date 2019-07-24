122cd68dd00762da539cf25d05adf05a2e6f17fb
Peter Boling <peter.boling@gmail.com>
2019-07-24 00:24:26 -0700

    Allow specification on command line

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require 'colorized_string'
 
 seed_size = ENV['SEED_SIZE'] ? ENV['SEED_SIZE'].to_i : 1_000_000
 sequential_days = ENV['SEQ_DAYS'] ? ENV['SEQ_DAYS'].to_i : 30
+slice_size = ENV['SEED_SLICE_SIZE'] ? ENV['SEED_SLICE_SIZE'].to_i : 10_000
 
 data_list = DataList.seeder(
   size: seed_size,
@@ -13,7 +14,7 @@ data_list = DataList.seeder(
 
 red_dot = ColorizedString['.'].red.on_blue
 puts "Importing #{seed_size} PageViews, each '#{red_dot}' represents 10,000\n"
-data_list.each.each_slice(10_000) do |list|
+data_list.each.each_slice(slice_size) do |list|
   PageView.import(
     DataList::COLUMNS,
     list,
