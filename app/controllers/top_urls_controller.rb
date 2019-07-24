6e2fd0a1c240fdaf043558b02e05ca8093c332db
Peter Boling <peter.boling@gmail.com>
2019-07-23 22:40:09 -0700

    Cleanup

--- app/controllers/top_urls_controller.rb
+++ app/controllers/top_urls_controller.rb
@@ -1,12 +1,6 @@
 class TopUrlsController < ApplicationController
   def index
-    posts = Post.order(created_at: :desc)
-    render json: { status: 'SUCCESS', message: 'loaded posts', data: posts }
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title)
+    top_urls = PageView.order(created_at: :desc)
+    render json: { }
   end
 end
