284fe0aebf736bc17e55ce71108a6dc2fde0c3a4
Peter Boling <peter.boling@gmail.com>
2019-08-08 09:50:18 -0400

    Fix spec grammar

--- spec/services/alexa_top_domains_spec.rb
+++ spec/services/alexa_top_domains_spec.rb
@@ -1,43 +1,51 @@
 # frozen_string_literal: true
 
 RSpec.describe AlexaTopDomains do
-  let(:instance) { described_class.new }
+  subject(:instance) { described_class.new }
 
   describe '#initialize' do
-    subject { instance }
-
     it 'does not raise' do
       VCR.use_cassette('alexa_top_domains') do
         block_is_expected.not_to raise_error
       end
     end
-    context 'initial values' do
-      describe 'domains' do
-        subject { instance.domains }
+    describe 'domains' do
+      subject(:domains) { instance.domains }
 
-        it 'is an enumerator' do
+      it 'is an enumerator' do
+        VCR.use_cassette('alexa_top_domains') do
+          is_expected.to be_a(Enumerator)
+        end
+      end
+
+      context 'when size arg is not provided' do
+        it 'size is DEFAULT_SIZE' do
           VCR.use_cassette('alexa_top_domains') do
-            is_expected.to be_a(Enumerator)
+            expect(domains.size).to eq(described_class::DEFAULT_SIZE)
           end
         end
-        context 'default' do
-          let(:instance) { described_class.new(size: nil) }
+      end
 
-          it 'size is DEFAULT_SIZE' do
-            VCR.use_cassette('alexa_top_domains') do
-              expect(instance.domains.size).to eq(described_class::DEFAULT_SIZE)
-            end
+      context 'when size arg is nil' do
+        subject(:domains) { described_class.new(size: size).domains }
+
+        let(:size) { nil }
+
+        it 'size is DEFAULT_SIZE' do
+          VCR.use_cassette('alexa_top_domains') do
+            expect(domains.size).to eq(described_class::DEFAULT_SIZE)
           end
         end
+      end
 
-        context 'large' do
-          let(:size) { 1_111_111 }
-          let(:instance) { described_class.new(size: size) }
+      context 'when size arg is large' do
+        subject(:domains) { described_class.new(size: size).domains }
 
-          it 'size is large' do
-            VCR.use_cassette('alexa_top_domains') do
-              expect(instance.domains.size).to eq(size)
-            end
+        let(:size) { 1_111_111 }
+
+        it 'size is large' do
+          VCR.use_cassette('alexa_top_domains') do
+            expect(domains.size).to eq(size)
           end
         end
       end
