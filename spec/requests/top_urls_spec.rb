351f02995525208e7759fe3b88ab2330038ac8b4
Peter Boling <peter.boling@gmail.com>
2019-08-08 10:04:39 -0400

    Fix spec grammar

--- spec/requests/top_urls_spec.rb
+++ spec/requests/top_urls_spec.rb
@@ -7,10 +7,15 @@ RSpec.describe 'Top URLs', type: :request do
     }
   end
 
-  it 'is successful' do
+  it 'has content type of JSON' do
     get '/top_urls', headers: headers
 
     expect(response.content_type).to eq('application/json')
+  end
+
+  it 'is successful' do
+    get '/top_urls', headers: headers
+
     expect(response).to have_http_status(:ok)
   end
   # NOTE: The DB must be seeded every day, to keep the leading edge of the data fresh
@@ -22,6 +27,7 @@ RSpec.describe 'Top URLs', type: :request do
   #   RAILS_ENV=test bin/rake db:seed
   #   RAILS_ENV=test bin/rake db:reset
   #
+  # rubocop:disable RSpec/MultipleExpectations, RSpec/ExampleLength
   it 'returns data' do
     get '/top_urls', headers: headers
 
@@ -36,6 +42,7 @@ RSpec.describe 'Top URLs', type: :request do
     expect(first_day_result - second_day_result).to eq(1)
     expect(first_day_result - third_day_result).to eq(2)
     expect(first_day_result - fourth_day_result).to eq(3)
-    expect(first_day_result - last_day_result).to eq(4)
+    expect(first_day_result - last_day_result).to eq(4) # 4 days between first and last day, for 5 full days total
   end
+  # rubocop:enable RSpec/MultipleExpectations, RSpec/ExampleLength
 end
